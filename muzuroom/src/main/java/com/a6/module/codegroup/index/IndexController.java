4cff4c4759c2cf2e81086e63254c339a36dc7da4
AhnSaeChan <sechan8896@gmail.com>
2025-03-13 17:48:49 +0900

    20250313

---
+++ muzuroom/src/main/java/com/a6/module/codegroup/index/IndexController.java
@@ -0,0 +1,8 @@
+package com.a6.module.codegroup.index;
+
+public class IndexController {
+	@RequestMapping(value = {"/indexUsrView"})
+	public String indexUsrView() throws Exception {
+		return pathCommonUsr + "indexUsrView";
+	}
+}
